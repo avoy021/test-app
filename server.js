620d9fd177b6218839ffa8213a3f96fbdcc97c0d
avoy <avoydas0506@gmail.com>
2023-09-25 21:02:57 +0530

    server.js changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.json({message: 'Success'});
+    res.json({message: 'Change was made'});
 })
 
 app.listen(3000, () => console.log('Server is running at PORT 3000'))
