bc8c684dc52f07858424953e8ca5bb65ff03c8e5
avoy <avoydas0506@gmail.com>
2023-09-25 21:23:41 +0530

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.json({message: 'deploying using one single command'});
+    res.json({message: 'deploying using one single commandddd'});
 })
 
 app.listen(3000, () => console.log('Server is running at PORT 3000'))
