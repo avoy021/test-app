1047a9721f023cfdaabcfeb69a9631c39d4f424d
avoy <avoydas0506@gmail.com>
2023-09-25 21:49:19 +0530

    files changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("heeloo changes");
+    res.send("github actions");
 })
 
 app.listen(3000, () => console.log('Server is running at PORT 3000'))
